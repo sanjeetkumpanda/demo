c576bc8971339d9fde8eee9ff4187754deb4efa2
Sanjeet <sanjeet4soft@gmail.com>
2019-03-28 18:55:08 +0530

    Java Pattern Matching

---
+++ SeleniumPractice/src/com/selenium/Pattern1.java
@@ -0,0 +1,15 @@
+package com.selenium;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		
+	
+	System.out.println("Printing the Pattern-1");
+	for(int i=1;i<4;i++) {
+		for(int j=1;j<4;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	}
+}
