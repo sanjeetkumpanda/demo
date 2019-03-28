04b3df8d39e3008327ba5a8b3ad8e2ef8e73abfc
Sanjeet <sanjeet4soft@gmail.com>
2019-03-28 18:57:51 +0530

    Java Program commiting

--- SeleniumPractice/src/com/selenium/Pattern1.java
+++ SeleniumPractice/src/com/selenium/Pattern1.java
@@ -3,7 +3,6 @@ package com.selenium;
 public class Pattern1 {
 	public static void main(String[] args) {
 		
-	
 	System.out.println("Printing the Pattern-1");
 	for(int i=1;i<4;i++) {
 		for(int j=1;j<4;j++) {
